36a3f03ea41903ea25bed51e6aa1cca7ba2ba1ca
Anto Wiranto <antowrnto11@gmail.com>
2022-06-17 03:42:41 +0000

    WIP

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+| 
 */
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
